e94396a060af99ec268a5217a9ab826e03e1736d
Yokimitsuro <gonzalezciclo@gmail.com>
2026-06-29 19:17:58 +0200

    decomp: close ov134 ov135 ov136

---
+++ src/calls/func_ov134_020cd9d8.c
@@ -0,0 +1,39 @@
+extern void func_0203c634(void);
+extern void func_ov107_020c9264(void);
+extern void func_ov107_020c9ee8(void);
+extern void func_ov134_020cda54(void);
+
+asm void func_ov134_020cd9d8(void)
+{
+    dcd     0xe92d4038
+    dcd     0xe1a04000
+    dcd     0xe5945004
+    dcd     0xe5950004
+    dcd     0xe5d000ad
+    dcd     0xe3500000
+    dcd     0x18bd8038
+    dcd     0xe5950000
+    dcd     0xe3a01005
+    dcd     0xe3a02000
+    bl      func_ov107_020c9264
+    dcd     0xe5950000
+    dcd     0xe3a01001
+    dcd     0xe59003a0
+    dcd     0xe3a02000
+    bl      func_ov107_020c9ee8
+    dcd     0xe595c000
+    dcd     0xe59f202c
+    dcd     0xe1dc36b0
+    dcd     0xe1a00004
+    dcd     0xe1a01803
+    dcd     0xe1a01c21
+    dcd     0xe3811040
+    dcd     0xe3c33cff
+    dcd     0xe1a01c01
+    dcd     0xe1831821
+    dcd     0xe1cc16b0
+    dcd     0xe1d412d0
+    bl      func_0203c634
+    dcd     0xe8bd8038
+    dcd     func_ov134_020cda54
+}
---
+++ src/calls/func_ov134_020cdd34.c
@@ -0,0 +1,28 @@
+extern void func_0203c634(void);
+
+asm void func_ov134_020cdd34(void)
+{
+    dcd     0xe92d4008
+    dcd     0xe5903004
+    dcd     0xe5932000
+    dcd     0xe5d2117a
+    dcd     0xe1a01f81
+    dcd     0xe1b01fa1
+    dcd     0x08bd8008
+    dcd     0xe5d31042
+    dcd     0xe1a01f81
+    dcd     0xe1b01fa1
+    dcd     0x0a000005
+    dcd     0xe3a01004
+    dcd     0xe5c211c7
+    dcd     0xe1d012d0
+    dcd     0xe3a02000
+    bl      func_0203c634
+    dcd     0xe8bd8008
+    dcd     0xe3a01002
+    dcd     0xe5c211c7
+    dcd     0xe1d012d0
+    dcd     0xe3a02000
+    bl      func_0203c634
+    dcd     0xe8bd8008
+}
