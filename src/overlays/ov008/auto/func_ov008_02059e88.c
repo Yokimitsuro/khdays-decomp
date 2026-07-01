07732392a3646b2922b617c74e01bdbb38721d98
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-01 22:26:50 +0200

    ov002/008/025/235/255/257: decompile struct-copy blob-gap carves
    Seven reloc-free block-copy stubs matched byte-exact via typedef-struct
    assignment (Blk48 12-word / Blk44 11-word) reproducing mwcc ldm/stm codegen:
    - ov002/008/025 SetMatrix43: copy 48B MtxFx43 into obj+0x10c
    - ov235/255/257 CopyBlock44: copy 44B block into (*node)+0x30
    - ov235 CopyBlock44Alt: ldmib variant sourcing node+4/node+8
    
    Also fix find_asm_stub_candidates.py to unpack classify_functions' 3-tuple.

---
+++ src/overlays/ov008/auto/func_ov008_02059e88.c
@@ -0,0 +1,5 @@
+typedef struct { int w[12]; } Blk48;
+
+void func_ov008_02059e88(char *obj, void *src) {
+    *(Blk48 *)(obj + 0x10c) = *(Blk48 *)src;
+}
---
+++ src/overlays/ov257/auto/func_ov257_020d2614.c
@@ -0,0 +1,8 @@
+typedef struct { int w[11]; } Blk44;
+
+void func_ov257_020d2614(char *obj) {
+    char *node = *(char **)(obj + 4);
+    char *dst = *(char **)node;
+    char *src = *(char **)(node + 4);
+    *(Blk44 *)(dst + 0x30) = *(Blk44 *)src;
+}
