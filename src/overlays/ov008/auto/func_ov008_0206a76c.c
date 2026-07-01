38dd379fe291d38ddfcad91f716985311487c0fd
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 00:35:10 +0200

    overlays: decompile ClampWrapIndex family (2 funcs)

---
+++ src/overlays/ov008/auto/func_ov008_0206a76c.c
@@ -0,0 +1,9 @@
+int func_ov008_0206a76c(int a, int b, int c) {
+    if (a < b) {
+        a = (short)(c - 1);
+    }
+    if (a >= c) {
+        a = b;
+    }
+    return a;
+}
---
+++ src/overlays/ov025/auto/func_ov025_0209c83c.c
@@ -0,0 +1,9 @@
+int func_ov025_0209c83c(int a, int b, int c) {
+    if (a < b) {
+        a = (short)(c - 1);
+    }
+    if (a >= c) {
+        a = b;
+    }
+    return a;
+}
