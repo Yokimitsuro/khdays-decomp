8f4b648011dac1c86c40b3cac5123f7debcc7173
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 00:59:11 +0200

    overlays: decompile CompareByField6ThenField18 family (2 funcs)

---
+++ src/overlays/ov008/auto/func_ov008_0205a890.c
@@ -0,0 +1,7 @@
+int func_ov008_0205a890(char *a, char *b) {
+    unsigned short hb = *(unsigned short *)(b + 6);
+    unsigned short ha = *(unsigned short *)(a + 6);
+    if (ha > hb) return 1;
+    if (ha < hb) return 0;
+    return *(int *)(a + 0x18) > *(int *)(b + 0x18);
+}
