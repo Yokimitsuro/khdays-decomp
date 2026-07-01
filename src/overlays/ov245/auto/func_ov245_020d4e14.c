dc7048568b21ecc217096caa1b660a34efca5230
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-01 23:21:28 +0200

    decomp: SetPendingState2, IsField38NibbleZero, SetState6cReset70 families (9 byte-exact)

---
+++ src/overlays/ov245/auto/func_ov245_020d4e14.c
@@ -0,0 +1,4 @@
+void func_ov245_020d4e14(char *obj) {
+    char *p = *(char **)obj;
+    *(char *)(p + 0x1c7) = 2;
+}
---
+++ src/overlays/ov267/auto/func_ov267_020d11e8.c
@@ -0,0 +1,4 @@
+void func_ov267_020d11e8(char *obj) {
+    *(int *)(obj + 0x6c) = 1;
+    *(int *)(obj + 0x70) = -1;
+}
