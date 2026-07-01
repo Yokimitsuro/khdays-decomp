072c72ad2b8eb0bf458acc6f28cd2af9b83b6102
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-01 23:49:07 +0200

    overlays: decompile GetField84Bit1 family (3 funcs)
    Byte-exact carve of the reloc-free bit-1 extractor at field +0x84
    across ov005, ov008, ov025.

---
+++ src/overlays/ov005/auto/func_ov005_0204e14c.c
@@ -0,0 +1,3 @@
+int func_ov005_0204e14c(int a, char *obj) {
+    return (int)(((unsigned)*(int *)(obj + 0x84) << 30) >> 31);
+}
---
+++ src/overlays/ov008/auto/func_ov008_02054810.c
@@ -0,0 +1,3 @@
+int func_ov008_02054810(int a, char *obj) {
+    return (int)(((unsigned)*(int *)(obj + 0x84) << 30) >> 31);
+}
