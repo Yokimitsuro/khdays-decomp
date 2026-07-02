f9dc6ca3c4a5c0caa1cd329804431272fc141752
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 22:06:25 +0200

    match ov139/140 + cab14 family (acquire-target divide callbacks)

---
+++ src/overlays/ov140/calls/func_ov140_020d0b74.c
@@ -0,0 +1,20 @@
+extern int func_ov107_020cab14(int a, int b);
+extern void func_ov107_020c9264();
+extern void func_0203c634(void *obj, int idx, void *value);
+extern void func_ov140_020d0c08(void);
+void func_ov140_020d0b74(int *node) {
+    int *state = (int *)node[1];
+    int t = func_ov107_020cab14(*state, 0);
+    state[1] = t;
+    if (t == 0) {
+        *(signed char *)(*state + 0x1c7) = 2;
+        func_0203c634(node, *(signed char *)(node + 8), 0);
+        return;
+    }
+    {
+        int v = *(int *)(*node + 0x2c) * 0x1e;
+        state[4] = v / 10;
+    }
+    func_ov107_020c9264(*state, 2, 0);
+    func_0203c634(node, *(signed char *)(node + 8), func_ov140_020d0c08);
+}
