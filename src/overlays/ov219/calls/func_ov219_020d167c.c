522dad9ca4159f0aeeaff44cfc9ed61166cc9541
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 16:20:41 +0200

    match 5 more state-transition callbacks (field-set/guard/countdown shapes)

---
+++ src/overlays/ov219/calls/func_ov219_020d167c.c
@@ -0,0 +1,7 @@
+extern void func_0203c634();
+void func_ov219_020d167c(int node) {
+    int *s = *(int **)(node + 4);
+    s[8] = 0x480;
+    *(signed char *)(*s + 0x1c7) = 4;
+    func_0203c634(node, *(signed char *)(node + 0x20), 0);
+}
---
+++ src/overlays/ov292/calls/func_ov292_020d455c.c
@@ -0,0 +1,5 @@
+extern void func_0203c634();
+void func_ov292_020d455c(int node) {
+    *(signed char *)(*(int *)*(int *)(node + 4) + 0x1c7) = 4;
+    func_0203c634(node, *(signed char *)(node + 0x20), 0);
+}
