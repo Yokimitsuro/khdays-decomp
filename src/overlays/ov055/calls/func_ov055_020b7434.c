fe984fee316c15df12792aa4147852e160205043
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 19:59:18 +0200

    match 7 thumb 2-call callbacks (ov037/055/056/075/076/092/093 family)

---
+++ src/overlays/ov055/calls/func_ov055_020b7434.c
@@ -0,0 +1,5 @@
+extern void func_ov022_02091228();extern void func_020236ac();
+void func_ov055_020b7434(int p) {
+    func_ov022_02091228(*(int *)(p + 0x2644));
+    func_020236ac(*(int *)(p + 0x2644));
+}
