b19b4087717c6f036c7ffc555aebb530d11d7a7e
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 19:52:23 +0200

    match 3 ov239-family state callbacks (AND-flag clear + status + transition)

---
+++ src/overlays/ov240/calls/func_ov240_020cedbc.c
@@ -0,0 +1,8 @@
+extern void func_0203c634();
+void func_ov240_020cedbc(int node) {
+    int *s = *(int **)(node + 4);
+    if (*(unsigned char *)(s[1] + 0xad) != 0) return;
+    *(unsigned short *)(*s + 0x1ae) &= ~1;
+    *(signed char *)(*s + 0x1c7) = 2;
+    func_0203c634(node, *(signed char *)(node + 0x20), 0);
+}
