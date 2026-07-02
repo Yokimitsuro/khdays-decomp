b563454cc99742f8f9ebc40c4d7f1a0d44e046c4
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 20:08:52 +0200

    match 2 thumb 5-call spawn callbacks (ov037/038); x35 family bulk-verify flaky, deferred

---
+++ src/overlays/ov037/calls/func_ov037_020b3234.c
@@ -0,0 +1,8 @@
+extern int func_02023c00();extern void func_ov037_020b3280();extern void func_ov037_020b3b98();extern void func_ov022_020a4798();extern void func_ov022_0209fb24(void);
+int func_ov037_020b3234(int p) {
+    int r = func_02023c00(p);
+    func_ov037_020b3280(p);
+    func_ov037_020b3b98(r);
+    func_ov022_020a4798(r, 0x46, 0xcf);
+    return (int)func_ov022_0209fb24;
+}
