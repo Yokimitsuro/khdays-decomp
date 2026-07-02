08e2c8e78e19498e9d475229feb2edc0f112922e
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 14:59:03 +0200

    match func_ov131_020cd86c: node state raise-flag/begin-anim callback

---
+++ src/overlays/ov131/calls/func_ov131_020cd86c.c
@@ -0,0 +1,15 @@
+extern void func_ov107_020c9ee8();
+extern void func_ov107_020c9264();
+extern void func_0203c634(void *obj, int idx, void *value);
+extern void func_ov131_020cd8d8(void);
+
+void func_ov131_020cd86c(char *obj) {
+    int *state = *(int **)(obj + 4);
+    unsigned int h = *(unsigned short *)(*state + 0x60);
+    unsigned int masked = h & ~0xff00;
+    *(unsigned short *)(*state + 0x60) =
+        masked | (((((h << 0x10) >> 0x18) | 0x40) << 0x18) >> 0x10);
+    func_ov107_020c9ee8(*(void **)(*state + 0x3c8), 0, 0, masked);
+    func_ov107_020c9264(*state, 4, 0);
+    func_0203c634(obj, *(signed char *)(obj + 0x20), func_ov131_020cd8d8);
+}
