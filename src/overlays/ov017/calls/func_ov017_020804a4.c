92ce52a156e612565c2297a5d7162a9e82c7873f
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 20:11:32 +0200

    match 2 thumb wrappers (ov021 tail-veneer, ov017 setter)

---
+++ src/overlays/ov017/calls/func_ov017_020804a4.c
@@ -0,0 +1,5 @@
+extern void func_0202b450(int a, int b);
+void func_ov017_020804a4(int p, int b, int c) {
+    func_0202b450(p + 0x28, b);
+    *(short *)(p + 0x18) = (short)c;
+}
---
+++ src/overlays/ov021/calls/func_ov021_02080650.c
@@ -0,0 +1,4 @@
+extern void *func_0202bc30();
+void *func_ov021_02080650(int p) {
+    return func_0202bc30(p + 0x124);
+}
