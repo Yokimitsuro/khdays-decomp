0ac64530a39957e985c57e0adfe8e0391bc9f179
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 19:58:11 +0200

    match 2 thumb wrappers (ov012: void-call, bool-forward)

--- src/overlays/ov012/asm_stubs/calls/func_ov012_0205ba3c.c
+++
@@ -1,9 +0,0 @@
-extern void func_ov012_0205b8e0(void);
-
-asm void func_ov012_0205ba3c(void)
-{
-    push    {r3, lr}
-    bl      func_ov012_0205b8e0
-    mov     r0, #0
-    pop     {r3, pc}
-}
---
+++ src/overlays/ov012/calls/func_ov012_0205ba48.c
@@ -0,0 +1,5 @@
+extern int func_ov012_0205b8fc();
+int func_ov012_0205ba48(void) {
+    if (func_ov012_0205b8fc() != 0) return 1;
+    return 0;
+}
