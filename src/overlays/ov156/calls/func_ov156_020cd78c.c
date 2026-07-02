30c7dc9e752351a60b9f9538195400b5e5f471bf
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 21:39:14 +0200

    match 5 more divide+guard callbacks (ov156/157/191/192/193)

---
+++ src/overlays/ov156/calls/func_ov156_020cd78c.c
@@ -0,0 +1,9 @@
+extern void func_0203c634(void *obj, int idx, void *value);
+void func_ov156_020cd78c(int *node) {
+    int v = *(int *)(*node + 0x2c) * 0x1e;
+    int *state = (int *)node[1];
+    state[12] = v / 5;
+    if (*(unsigned char *)state[1] != 0) return;
+    *(signed char *)(*state + 0x1c7) = 2;
+    func_0203c634(node, *(signed char *)(node + 8), 0);
+}
