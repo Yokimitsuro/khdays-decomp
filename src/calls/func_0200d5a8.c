7e8fdc13e92d7b7138d5da854202f10d51d0bcfe
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 15:58:00 +0200

    match func_0200d5a8/5c0/5d8/5f0 tail-call family + func_0201231c setter

---
+++ src/calls/func_0200d5a8.c
@@ -0,0 +1,5 @@
+extern void func_0200d4d8();
+extern int data_020463d8;
+void func_0200d5a8(int param_1) {
+    func_0200d4d8(&data_020463d8, param_1);
+}
---
+++ src/calls/func_0200d5c0.c
@@ -0,0 +1,5 @@
+extern void func_0200d4ec();
+extern int data_020463e4;
+void func_0200d5c0(int param_1) {
+    func_0200d4ec(&data_020463e4, param_1);
+}
