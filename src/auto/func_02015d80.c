140ea8028ec9eff41226a3e370061cd7dd34d304
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 08:05:09 +0200

    decomp: AccumulateVec3Scaled (main)

---
+++ src/auto/func_02015d80.c
@@ -0,0 +1,11 @@
+void func_02015d80(int *a, int *b, int scale, int flag) {
+    if (flag) {
+        a[0] += scale;
+        a[1] += scale;
+        a[2] += scale;
+    } else {
+        a[0] += (scale * b[0]) >> 12;
+        a[1] += (scale * b[1]) >> 12;
+        a[2] += (scale * b[2]) >> 12;
+    }
+}
