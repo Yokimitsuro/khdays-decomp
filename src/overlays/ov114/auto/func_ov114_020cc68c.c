b3858ce824a61d508f5d7235db74aad46eff8d8d
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 08:03:36 +0200

    decomp: UpdateNodeReservationState2 (ov114/ov244/ov277)

---
+++ src/overlays/ov114/auto/func_ov114_020cc68c.c
@@ -0,0 +1,17 @@
+typedef struct { unsigned char flag : 1; } BitByte;
+
+int func_ov114_020cc68c(char *obj) {
+    char *node = *(char **)*(char **)(obj + 0x214);
+    signed char state = *(signed char *)(node + 0x1c6);
+    if (state == 0xc) {
+        *(char *)(node + 0x1c7) = 0;
+        return 1;
+    }
+    if (*(signed char *)(node + 0x1c7) != 0xb && state != 0xb) {
+        if (((BitByte *)(node + 0x17a))->flag) {
+            if (!(state != 2 && state != 4 && state != 9 && state != 0xa))
+                *(char *)(node + 0x1c7) = 0xb;
+        }
+    }
+    return 0;
+}
