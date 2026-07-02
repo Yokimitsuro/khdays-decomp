e2c93b141cfbddb9de22996355def564b50cad55
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 20:13:53 +0200

    match func_ov098_020ba7fc (thumb 4-call family, missed in prior commit)

---
+++ src/overlays/ov098/calls/func_ov098_020ba7fc.c
@@ -0,0 +1,7 @@
+extern int func_02023c00();extern void func_ov098_020bb358();extern void func_ov022_0209fab4();extern int data_ov098_020bbda0;
+void func_ov098_020ba7fc(int p) {
+    int r = func_02023c00(p);
+    func_ov098_020bb358(r);
+    func_ov022_0209fab4(r);
+    *(int *)&data_ov098_020bbda0 = 0;
+}
