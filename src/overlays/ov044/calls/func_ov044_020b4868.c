6f28c7eb60b87cbae9e73dc1250dfe5bdc1ce09e
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 19:57:26 +0200

    match 5 thumb 3-call state callbacks (ov030/044/063 family)

---
+++ src/overlays/ov044/calls/func_ov044_020b4868.c
@@ -0,0 +1,6 @@
+extern void func_0202a7dc();extern void func_0202a7dc();extern void func_ov044_020b3b10();
+void func_ov044_020b4868(int p) {
+    func_0202a7dc(p + 0x18);
+    func_0202a7dc(p + 0x128);
+    func_ov044_020b3b10(p);
+}
---
+++ src/overlays/ov082/calls/func_ov082_020b9748.c
@@ -0,0 +1,6 @@
+extern void func_0202a7dc();extern void func_0202a7dc();extern void func_ov082_020b89f0();
+void func_ov082_020b9748(int p) {
+    func_0202a7dc(p + 0x18);
+    func_0202a7dc(p + 0x128);
+    func_ov082_020b89f0(p);
+}
