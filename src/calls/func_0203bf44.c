9567ba661c1b31cea18648cb86cc66548e373fd6
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-02 12:57:50 +0200

    match func_02029250, func_0203c9b4, func_0203bf44
    func_02029250 forwards param_2 to func_02029158 when [+0x9c] is set;
    func_0203c9b4 calls func_0202f188 then clears bit0 of byte [+0x28];
    func_0203bf44 allocates 0xb4 via func_0203d15c, inits with func_0203bf60.

---
+++ src/calls/func_0203bf44.c
@@ -0,0 +1,8 @@
+extern void *func_0203d15c(int);
+extern void func_0203bf60(void *);
+
+void *func_0203bf44(void) {
+    void *obj = func_0203d15c(0xb4);
+    func_0203bf60(obj);
+    return obj;
+}
---
+++ src/calls/func_0203c9b4.c
@@ -0,0 +1,6 @@
+extern void func_0202f188(void *);
+
+void func_0203c9b4(void *param_1) {
+    func_0202f188(param_1);
+    *((unsigned char *)param_1 + 0x28) &= ~1;
+}
