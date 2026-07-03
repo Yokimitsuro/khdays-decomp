4eccb9020250ed70fbf91282808c67d65ac13009
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-03 21:41:34 +0200

    Match freeSubObjectChannels family (ov035_020b423c +3): release sub-object channels + NNSi_FndFreeFromDefaultHeap

---
+++ src/overlays/ov074/calls/func_ov074_020b911c.c
@@ -0,0 +1,7 @@
+extern void func_ov022_02091228(int a);
+extern void NNSi_FndFreeFromDefaultHeap(int a);
+void func_ov074_020b911c(int this) {
+    func_ov022_02091228(*(int *)(this + 0x2644));
+    func_ov022_02091228(*(int *)(this + 0x2644) + 0x30);
+    NNSi_FndFreeFromDefaultHeap(*(int *)(this + 0x2644));
+}
