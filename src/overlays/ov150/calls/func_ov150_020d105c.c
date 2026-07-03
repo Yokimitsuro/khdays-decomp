2e08e6d48f83a912b405d58427dc68594b02fd64
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-03 14:07:45 +0200

    Match stAimAngleToTarget family (ov141_020cd418 +4): VEC_Subtract + angle calc + transition (VEC_Subtract canonical)

---
+++ src/overlays/ov150/calls/func_ov150_020d105c.c
@@ -0,0 +1,24 @@
+extern void func_ov107_020c9264();
+extern void VEC_Subtract(void *a, void *b, void *out);
+extern int func_020050b4(int x, int z);
+extern void func_ov107_020c9ee8();
+extern void func_0203c634(void *obj, int idx, void *value);
+extern void func_ov150_020d10e0(void);
+
+void func_ov150_020d105c(int *node) {
+    int *state = (int *)node[1];
+    int buf[3];
+    func_ov107_020c9264(*state, 3, 0);
+    {
+        int p = state[0xe];
+        if (p != 0) {
+            int r;
+            VEC_Subtract((void *)(p + 0x190), (void *)(*state + 0xb0), buf);
+            r = func_020050b4(buf[0], buf[2]);
+            state[3] = r;
+            state[2] = r;
+        }
+    }
+    func_ov107_020c9ee8(*(int *)(*state + 0x3cc), 0, 0);
+    func_0203c634(node, *(signed char *)(node + 8), func_ov150_020d10e0);
+}
