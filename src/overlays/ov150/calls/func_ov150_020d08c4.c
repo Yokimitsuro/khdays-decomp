b7e24c96e0a31bdcba4324a23d4ab6803b3b9698
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-03 13:59:35 +0200

    Match stateSetFlagsEffectClear82 family (ov141_020ccc80 +4): hw60|=0x82 + u16[0x1ae]|=1 + bf clear + effect + transition

---
+++ src/overlays/ov150/calls/func_ov150_020d08c4.c
@@ -0,0 +1,18 @@
+struct bf { unsigned b : 8; };
+extern void func_ov107_020c5af8();
+extern void func_0203c634(void *obj, int idx, void *value);
+extern void func_ov150_020d0960(void);
+
+void func_ov150_020d08c4(int *node) {
+    int *state = (int *)node[1];
+    {
+        unsigned short *p = (unsigned short *)(*state + 0x60);
+        unsigned int u = *p;
+        *p = (unsigned short)((u & ~0xff00) | ((((u << 0x10) >> 0x18 | 0x82) << 0x18) >> 0x10));
+    }
+    *(unsigned short *)(*state + 0x1ae) |= 1;
+    ((struct bf *)(*(int *)(*state + 0x388) + 8))->b &= ~1;
+    state[0xc] = 0;
+    func_ov107_020c5af8(*state, 0, 0x48, state[0x10]);
+    func_0203c634(node, *(signed char *)(node + 8), func_ov150_020d0960);
+}
