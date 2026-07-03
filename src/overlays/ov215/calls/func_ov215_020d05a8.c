12fd13ad391f788406523b2df0ac12b2185e1789
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-03 17:30:53 +0200

    Match initChildProjectile family (ov214_020cc968 +4): 4-arg child projectile setup

---
+++ src/overlays/ov215/calls/func_ov215_020d05a8.c
@@ -0,0 +1,17 @@
+extern void func_0202a440(void *p);
+extern void func_02014b5c(void *a, int b);
+extern void func_0202a388(void *a, void *b, int c, int d);
+extern void func_0203b9ac(void *a, void *b);
+extern void func_0203b9fc(void *a, int b, int c, int d);
+extern void func_0203c7ac(void *a, int b);
+
+void func_ov215_020d05a8(char *this, int param2, int param3, char *param4) {
+    char *obj = *(char **)(this + 0x88);
+    func_0202a440(param4);
+    *(int *)(param4 + 0xc) = 0;
+    func_02014b5c(obj + 0x20, *(int *)(obj + 0x78));
+    func_0202a388(param4, obj, param2, 0xc);
+    func_0203b9ac(this, param4);
+    func_0203b9fc(this, 0, 0, param3);
+    func_0203c7ac(this, 0);
+}
