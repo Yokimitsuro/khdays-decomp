78ef9ac6c4f49b9a7a9623a538f737902e21b4ce
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-03 21:41:33 +0200

    Match 3 ov034 state-enter families (5164/51e0/5278 +9): mode-set + 3-field seed + anim-handler registration (states 5/1/3)

---
+++ src/overlays/ov034/calls/func_ov034_020b5164.c
@@ -0,0 +1,16 @@
+extern int func_01fff774(int, int, int);
+extern int func_02016ae8(int, int, int);
+
+void func_ov034_020b5164(int a, int *this) {
+    int i;
+    *(int *)((char *)this + 0x24) = 5;
+    func_01fff774((int)this + 0x28, 0, 0);
+    func_01fff774((int)this + 0x28, 2, 0);
+    *(int *)((char *)this + 0xe0) = 0xccd;
+    *(int *)((char *)this + 0xdc) = 0xccd;
+    *(int *)((char *)this + 0xd8) = 0xccd;
+    for (i = 0; i < 0xa; i++) {
+        func_02016ae8(*(int *)((char *)this + 0xa0), i, 0);
+    }
+    func_02016ae8(*(int *)((char *)this + 0xa0), 7, 3);
+}
---
+++ src/overlays/ov090/calls/func_ov090_020bc780.c
@@ -0,0 +1,18 @@
+extern int func_01fff774(int, int, int);
+extern int func_02016ae8(int, int, int);
+
+void func_ov090_020bc780(int a, int *this) {
+    int i;
+    *(int *)((char *)this + 0x24) = 1;
+    func_01fff774((int)this + 0x28, 0, 0);
+    func_01fff774((int)this + 0x28, 2, 0);
+    *(int *)((char *)this + 0xe0) = 0x1000;
+    *(int *)((char *)this + 0xdc) = 0x1000;
+    *(int *)((char *)this + 0xd8) = 0x1000;
+    for (i = 0; i < 7; i++) {
+        func_02016ae8(*(int *)((char *)this + 0xa0), i, 0);
+    }
+    func_02016ae8(*(int *)((char *)this + 0xa0), 7, 3);
+    func_02016ae8(*(int *)((char *)this + 0xa0), 8, 3);
+    func_02016ae8(*(int *)((char *)this + 0xa0), 9, 3);
+}
