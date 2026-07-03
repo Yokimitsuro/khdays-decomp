d4a9363c9aa5f4d62ba9f150cb07fb4d05abde51
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-03 14:23:20 +0200

    Match stInitSlotsFlags6B family (ov156_020ccd54 +4): state init + 3 SetIndexedSlot transitions

---
+++ src/overlays/ov193/calls/func_ov193_020d461c.c
@@ -0,0 +1,25 @@
+struct hw60 { unsigned short lo : 8, hi : 8; };
+struct bf { unsigned b : 8; };
+extern void func_0203c634(void *obj, int idx, void *value);
+extern void func_ov193_020d4714(void);
+extern void func_ov193_020d49b4(void);
+extern void func_ov193_020d490c(void);
+
+void func_ov193_020d461c(int *node) {
+    int *state = (int *)node[1];
+    *(signed char *)(*state + 0x1c6) = 0;
+    *(signed char *)(*state + 0x1c7) = -1;
+    ((struct bf *)(*(int *)(*state + 0x38c) + 8))->b &= ~1;
+    ((struct bf *)(*(int *)(*state + 0x388) + 8))->b &= ~1;
+    state[2] = *state + 0xb0;
+    state[3] = *state + 0x74;
+    state[1] = *(int *)(*state + 0x384) + 0xad;
+    {
+        unsigned short *p = (unsigned short *)(*state + 0x60);
+        unsigned int u = *p;
+        *p = (unsigned short)((u & ~0xff00) | ((((u << 0x10) >> 0x18 | 6) << 0x18) >> 0x10));
+    }
+    func_0203c634(node, 0, func_ov193_020d4714);
+    func_0203c634(node, 1, func_ov193_020d49b4);
+    func_0203c634(node, 2, func_ov193_020d490c);
+}
