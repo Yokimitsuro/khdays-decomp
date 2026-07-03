808b90f90a8f293a6bb10334ccde7af2faf9dc95
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-03 17:22:06 +0200

    Match stSetFlags86Effect4d (ov214_020ce684 +4) and stAimAtTargetOrIdle (ov214_020cda04 +1) families

---
+++ src/overlays/ov214/calls/func_ov214_020ce684.c
@@ -0,0 +1,19 @@
+struct hw60 { unsigned short lo : 8, hi : 8; };
+struct bf { unsigned b : 8; };
+extern void func_ov107_020c5af8();
+extern void func_0203c634(void *obj, int idx, void *value);
+extern void func_ov214_020ce748(void);
+
+void func_ov214_020ce684(int *node) {
+    int *state = (int *)node[1];
+    ((struct hw60 *)(*state + 0x60))->hi &= ~1;
+    *(unsigned short *)(*state + 0x1ae) |= 3;
+    {
+        unsigned short *p = (unsigned short *)(*state + 0x60);
+        unsigned int u = *p;
+        *p = (unsigned short)((u & ~0xff00) | ((((u << 0x10) >> 0x18 | 0x86) << 0x18) >> 0x10));
+    }
+    ((struct bf *)(*(int *)(*state + 0x3ac) + 8))->b &= ~1;
+    func_ov107_020c5af8(*state, 0, 0x4d, *state + 0x74);
+    func_0203c634(node, *(signed char *)(node + 8), func_ov214_020ce748);
+}
---
+++ src/overlays/ov215/calls/func_ov215_020d1644.c
@@ -0,0 +1,29 @@
+extern void func_0203c634(void *obj, int idx, void *value);
+extern void VEC_Subtract(void *a, void *b, void *out);
+extern int func_01ff8d18(void *v, void *v2);
+extern int func_020050b4(int x, int z);
+extern void func_ov107_020c9264();
+extern void func_ov107_020c5af8();
+extern void func_ov215_020d1700(void);
+
+void func_ov215_020d1644(int *node) {
+    int *state = (int *)node[1];
+    int buf[3];
+    int target = state[2];
+    if (target == 0) {
+        *(signed char *)(*state + 0x1c7) = 2;
+        func_0203c634(node, *(signed char *)(node + 8), (void *)0);
+        return;
+    }
+    VEC_Subtract((void *)(target + 0x190), (void *)(*state + 0xb0), buf);
+    buf[1] = 0;
+    state[0x12] = func_01ff8d18(buf, buf);
+    {
+        int angle = func_020050b4(buf[0], buf[2]);
+        state[0x13] = angle;
+        state[0x11] = angle;
+    }
+    func_ov107_020c9264(*state, 3, 0);
+    func_ov107_020c5af8(*state, 0x129, 6, state[4]);
+    func_0203c634(node, *(signed char *)(node + 8), func_ov215_020d1700);
+}
