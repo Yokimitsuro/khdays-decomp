e045e5300c8c94ff45e83767857cd9eadc259c7c
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-03 19:30:08 +0200

    Match freeSubitemChannelResources family (ov031_020b395c +3): channel destructor

---
+++ src/overlays/ov088/calls/func_ov088_020baefc.c
@@ -0,0 +1,20 @@
+extern void func_ov022_02091228(int a);
+extern void NNSi_FndFreeFromDefaultHeap(int a);
+extern void func_0202a7dc(int a);
+extern int data_ov088_020bc360;
+
+void func_ov088_020baefc(int param_1, int param_2, int param_3, int param_4) {
+    int base = data_ov088_020bc360 + 0x2c2c;
+    int i;
+    func_ov022_02091228(*(int *)(param_1 + 0x2644));
+    func_ov022_02091228(*(int *)(param_1 + 0x2644) + 0x30);
+    func_ov022_02091228(*(int *)(param_1 + 0x2644) + 0x60);
+    NNSi_FndFreeFromDefaultHeap(*(int *)(param_1 + 0x2644));
+    base += 0x14;
+    i = 0;
+    do {
+        func_0202a7dc(base);
+        i++;
+        base += 0x118;
+    } while (i < 2);
+}
