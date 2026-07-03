d6b7d36e96bb09d275e59eb1096a29152b760efb
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-03 16:45:57 +0200

    Match stSetFlags86Clear3ac family (ov214_020cd4bc +5): hw60|=0x86 + bf clear + transition

---
+++ src/overlays/ov216/calls/func_ov216_020cd4cc.c
@@ -0,0 +1,14 @@
+struct bf { unsigned b : 8; };
+extern void func_0203c634(void *obj, int idx, void *value);
+extern void func_ov216_020cd534(void);
+
+void func_ov216_020cd4cc(int *node) {
+    int *state = (int *)node[1];
+    {
+        unsigned short *p = (unsigned short *)(*state + 0x60);
+        unsigned int u = *p;
+        *p = (unsigned short)((u & ~0xff00) | ((((u << 0x10) >> 0x18 | 0x86) << 0x18) >> 0x10));
+    }
+    ((struct bf *)(*(int *)(*state + 0x3ac) + 8))->b &= ~1;
+    func_0203c634(node, *(signed char *)(node + 8), func_ov216_020cd534);
+}
