c9378518441087e664ffb94032501aec68ffe5b2
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-03 16:34:40 +0200

    Match stSetFlags86Clear (ov194_020ccc2c +4) and stTickAimTimerC (ov194_020ccda4 +4) families

---
+++ src/overlays/ov270/calls/func_ov270_020d44ac.c
@@ -0,0 +1,15 @@
+struct bf { unsigned b : 8; };
+extern void func_0203c634(void *obj, int idx, void *value);
+extern void func_ov270_020d451c(void);
+
+void func_ov270_020d44ac(int *node) {
+    int *state = (int *)node[1];
+    {
+        unsigned short *p = (unsigned short *)(*state + 0x60);
+        unsigned int u = *p;
+        *p = (unsigned short)((u & ~0xff00) | ((((u << 0x10) >> 0x18 | 0x86) << 0x18) >> 0x10));
+    }
+    ((struct bf *)(*(int *)(*state + 0x38c) + 8))->b &= ~1;
+    state[0x11] = 0;
+    func_0203c634(node, *(signed char *)(node + 8), func_ov270_020d451c);
+}
