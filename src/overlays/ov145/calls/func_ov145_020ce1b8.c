c5b8b12d26a89b7662b7ffa72ac51078c2270846
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-04 03:35:03 +0200

    Match ov144/ov145 actor-teardown family (2 funcs): releases 5 sub-objects + guarded free + final handler call

---
+++ src/overlays/ov145/calls/func_ov145_020ce1b8.c
@@ -0,0 +1,18 @@
+extern void func_0203c7e8();
+extern void func_ov107_020c9eac();
+extern void func_0203d194();
+extern void func_ov107_020c68ec();
+
+void func_ov145_020ce1b8(int this_) {
+    func_0203c7e8(*(int *)(this_ + 0x384));
+    func_ov107_020c9eac(*(int *)(this_ + 0x394));
+    func_0203c7e8(*(int *)(this_ + 0x388));
+    func_0203c7e8(*(int *)(this_ + 0x398));
+    func_0203c7e8(*(int *)(this_ + 0x3f8));
+    func_0203c7e8(*(int *)(this_ + 0x38c));
+    if (*(int *)(this_ + 0x39c) != 0) {
+        func_0203d194(*(int *)(this_ + 0x39c));
+        *(int *)(this_ + 0x39c) = 0;
+    }
+    func_ov107_020c68ec(this_);
+}
