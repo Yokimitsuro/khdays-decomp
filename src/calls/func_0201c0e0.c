84dc8346a3eaed896064942ff6aabbc5a44290b0
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-05 01:31:50 +0200

    Match func_0201c008/c074/c0e0 family: guarded create-with-callback (3 variants, differ by callback fn-ptr)

---
+++ src/calls/func_0201c0e0.c
@@ -0,0 +1,17 @@
+extern int func_0201b7e0(int handle);
+extern int func_0201b3d8(void);
+extern int func_0201befc(int handle, void *cb, int arg, int handle2, int *out);
+extern void func_0201b808(int handle, int entry);
+extern void func_0201c304(void);
+
+unsigned func_0201c0e0(unsigned handle, int *out, int flag) {
+    unsigned entry = func_0201b7e0(handle);
+    if (entry == 0) {
+        int arg;
+        if (flag == 0) arg = 0;
+        else arg = func_0201b3d8();
+        entry = func_0201befc(handle, func_0201c304, arg, handle, out);
+        if (flag != 0 && entry != 0) func_0201b808(handle, entry);
+    }
+    return entry;
+}
