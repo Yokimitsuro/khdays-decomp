e446555a106d4e2629aa9760db99e20077fb531e
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-04 03:15:42 +0200

    Match ov144/ov145 stamp-actor-bytes-then-forward family (2 funcs): msg-field copy from actor state + cross-section tail-call with pass-through 3rd arg

---
+++ src/overlays/ov144/calls/func_ov144_020cc5cc.c
@@ -0,0 +1,15 @@
+extern void *func_ov107_020c7a90();
+
+struct actor_1c6 {
+    signed char _pad[0x1c6];
+    signed char field_1c6;
+};
+
+void *func_ov144_020cc5cc(int this_, unsigned char *msg, int arg3) {
+    if (msg[2] == 0) {
+        msg[0x24] = ((struct actor_1c6 *)this_)->field_1c6;
+        msg[0x26] = *(int *)(this_ + 0x3f4);
+        msg[0x25] = *(int *)(this_ + 0x3bc);
+    }
+    return func_ov107_020c7a90(this_, msg, arg3);
+}
