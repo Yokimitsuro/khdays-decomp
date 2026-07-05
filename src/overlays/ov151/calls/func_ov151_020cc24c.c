d15aff17b0581367e95d11f31655b16b32d49b88
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-05 12:30:48 +0200

    Match 2 more sub-object teardown families (5 funcs)
    Via genfamily (DestroyInstance loops, row8-indexed):
    - ov191/192/193 020cff74 (72B): DestroyInstance(this->0x384); for(i<4)
      DestroyInstance(((row8*)*(this->0x3a0))[i].a); 2x func_0203d194; notify.
    - ov151/152 020cc24c (80B): DestroyInstance(this->0x384); func_ov107_020c9eac
      (this->0x3cc); for(i<5) DestroyInstance loop; 2x func_0203d194; notify.
    Byte-exact (masked).

---
+++ src/overlays/ov151/calls/func_ov151_020cc24c.c
@@ -0,0 +1,17 @@
+struct row8 { int a, b; };
+extern void func_0203c7e8(int p);
+extern void func_ov107_020c9eac(int p);
+extern void func_0203d194(int p);
+extern void func_ov107_020c68ec(int p);
+
+void func_ov151_020cc24c(int this_) {
+    int i;
+    func_0203c7e8(*(int *)(this_ + 0x384));
+    func_ov107_020c9eac(*(int *)(this_ + 0x3cc));
+    for (i = 0; i < 5; i++) {
+        func_0203c7e8(((struct row8 *)(*(int *)(this_ + 0x390)))[i].a);
+    }
+    func_0203d194(*(int *)(this_ + 0x390));
+    func_0203d194(*(int *)(this_ + 0x3c8));
+    func_ov107_020c68ec(this_);
+}
---
+++ src/overlays/ov193/calls/func_ov193_020d3bb4.c
@@ -0,0 +1,15 @@
+struct row8 { int a, b; };
+extern void func_0203c7e8(int p);
+extern void func_0203d194(int p);
+extern void func_ov107_020c68ec(int p);
+
+void func_ov193_020d3bb4(int this_) {
+    int i;
+    func_0203c7e8(*(int *)(this_ + 0x384));
+    for (i = 0; i < 4; i++) {
+        func_0203c7e8(((struct row8 *)(*(int *)(this_ + 0x3a0)))[i].a);
+    }
+    func_0203d194(*(int *)(this_ + 0x3a0));
+    func_0203d194(*(int *)(this_ + 0x3a4));
+    func_ov107_020c68ec(this_);
+}
