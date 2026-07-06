cedfef46e04d081dbd4fc33e1c16e489030e7bf0
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-06 08:00:20 +0200

    Match 18 hw60 action49 callbacks

---
+++ src/overlays/ov121/calls/func_ov121_020cfc50.c
@@ -0,0 +1,27 @@
+extern void func_ov107_020c5af8(int a, int b, int c, int d);
+extern void func_0203c634(void *node, int idx, void *value);
+
+struct ov121_Hw60 {
+    unsigned short lo : 8;
+    unsigned short hi : 8;
+};
+
+struct ov121_LowByteFlags {
+    unsigned bits : 8;
+};
+
+void func_ov121_020cfc50(int *node) {
+    int *state = (int *)node[1];
+    ((struct ov121_Hw60 *)(*state + 0x60))->hi &= ~1;
+
+    {
+        unsigned short hw60 = *(unsigned short *)(*state + 0x60);
+        *(unsigned short *)(*state + 0x60) =
+            (hw60 & ~0xff00) | (((((unsigned int)hw60 << 0x10) >> 0x18 | 0x86) << 0x18) >> 0x10);
+    }
+
+    ((struct ov121_LowByteFlags *)(*(int *)(*state + 0x388) + 8))->bits &= ~1;
+    func_ov107_020c5af8(*state, 0, 0x49, state[3]);
+    *(signed char *)(*state + 0x1c7) = 0;
+    func_0203c634(node, *(signed char *)((char *)node + 0x20), 0);
+}
---
+++ src/overlays/ov205/calls/func_ov205_020d639c.c
@@ -0,0 +1,27 @@
+extern void func_ov107_020c5af8(int a, int b, int c, int d);
+extern void func_0203c634(void *node, int idx, void *value);
+
+struct ov205_Hw60 {
+    unsigned short lo : 8;
+    unsigned short hi : 8;
+};
+
+struct ov205_LowByteFlags {
+    unsigned bits : 8;
+};
+
+void func_ov205_020d639c(int *node) {
+    int *state = (int *)node[1];
+    ((struct ov205_Hw60 *)(*state + 0x60))->hi &= ~1;
+
+    {
+        unsigned short hw60 = *(unsigned short *)(*state + 0x60);
+        *(unsigned short *)(*state + 0x60) =
+            (hw60 & ~0xff00) | (((((unsigned int)hw60 << 0x10) >> 0x18 | 0x86) << 0x18) >> 0x10);
+    }
+
+    ((struct ov205_LowByteFlags *)(*(int *)(*state + 0x388) + 8))->bits &= ~1;
+    func_ov107_020c5af8(*state, 0, 0x49, state[9]);
+    *(signed char *)(*state + 0x1c7) = 0;
+    func_0203c634(node, *(signed char *)((char *)node + 0x20), 0);
+}
