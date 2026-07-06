431fd07066005115008859db1916ea51293655d7
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-06 08:39:29 +0200

    Match 18 hw60 action48 callbacks

---
+++ src/overlays/ov204/calls/func_ov204_020d0860.c
@@ -0,0 +1,29 @@
+extern void func_ov107_020c5af8(int a, int b, int c, int d);
+extern void func_0203c634(void *node, int idx, void *value);
+extern void func_ov204_020d092c(void);
+
+struct ov204_Hw60 {
+    unsigned short lo : 8;
+    unsigned short hi : 8;
+};
+
+struct ov204_LowByteFlags {
+    unsigned bits : 8;
+};
+
+void func_ov204_020d0860(int *node) {
+    int *state = (int *)node[1];
+
+    {
+        unsigned short hw60 = *(unsigned short *)(*state + 0x60);
+        *(unsigned short *)(*state + 0x60) =
+            (hw60 & ~0xff00) | (((((unsigned int)hw60 << 0x10) >> 0x18 | 0x80) << 0x18) >> 0x10);
+    }
+
+    ((struct ov204_Hw60 *)(*state + 0x60))->hi &= ~0xc;
+    *(unsigned short *)(*state + 0x1ae) |= 1;
+    ((struct ov204_LowByteFlags *)(*(int *)(*state + 0x388) + 8))->bits &= ~1;
+    func_ov107_020c5af8(*state, 0, 0x48, state[9]);
+    state[11] = 0;
+    func_0203c634(node, *(signed char *)(node + 8), func_ov204_020d092c);
+}
