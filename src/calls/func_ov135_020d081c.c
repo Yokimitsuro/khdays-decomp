43b15a3a3461214341b1e7a5ea405509c66eab43
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-08 03:20:02 +0200

    Match 4 more dual-hw60 action48 callbacks (ov134/135/136/286)

---
+++ src/calls/func_ov135_020d081c.c
@@ -0,0 +1,22 @@
+extern void func_ov107_020c5af8(int a, int b, int c, int d);
+extern void func_0203c634(void *node, int idx, void *cb);
+extern void func_ov135_020d08e4(void);
+
+struct hw60 { unsigned short lo : 8, hi : 8; };
+struct ov135_LowByteFlags { unsigned bits : 8; };
+
+void func_ov135_020d081c(int *node) {
+    int *state = (int *)node[1];
+
+    {
+        unsigned short hw60 = *(unsigned short *)(*state + 0x60);
+        *(unsigned short *)(*state + 0x60) =
+            (hw60 & ~0xff00) | (((((unsigned int)hw60 << 0x10) >> 0x18 | 0x82) << 0x18) >> 0x10);
+    }
+    ((struct hw60 *)(*state + 0x60))->hi &= ~0xc;
+    *(unsigned short *)(*state + 0x1ae) |= 1;
+    ((struct ov135_LowByteFlags *)(*(int *)(*state + 0x38c) + 8))->bits &= ~1;
+    state[0xc] = 0;
+    func_ov107_020c5af8(*state, 0, 0x48, state[0xf]);
+    func_0203c634(node, *(signed char *)(node + 8), func_ov135_020d08e4);
+}
