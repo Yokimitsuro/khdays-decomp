9adcc8d8012646503f803978dc53408781cf9abc
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-08 04:12:18 +0200

    Match 4 node action-state callbacks (vec-copy + config-aim pairs, ov114-199 lane)

---
+++ src/calls/func_ov124_020d1778.c
@@ -0,0 +1,19 @@
+extern void func_ov107_020c5af8(int a, int b, int c, int d);
+extern void func_0203c634(void *node, int idx, void *cb);
+extern void func_ov124_020d1838(void);
+
+struct hw60 { unsigned short lo : 8, hi : 8; };
+struct ov124b_LowByteFlags { unsigned bits : 8; };
+struct vec3 { int a, b, c; };
+
+void func_ov124_020d1778(int *node) {
+    int *state = (int *)node[1];
+    state[9] = 0;
+    state[10] = 0;
+    ((struct hw60 *)(*state + 0x60))->hi &= ~0x9c;
+    *(unsigned short *)(*state + 0x1ae) &= ~3;
+    ((struct ov124b_LowByteFlags *)(*(int *)(*state + 0x388) + 8))->bits |= 1;
+    *(struct vec3 *)(state + 0xb) = *(struct vec3 *)((int *)state[2]);
+    func_ov107_020c5af8(*state, 0x115, 4, state[2]);
+    func_0203c634(node, *(signed char *)(node + 8), func_ov124_020d1838);
+}
---
+++ src/calls/func_ov174_020d20b8.c
@@ -0,0 +1,25 @@
+extern void VEC_Subtract(void *a, void *b, void *out);
+extern int func_01ff8d18(void *dst, void *src);
+extern void func_01ffa724(int scale, void *src, void *dst);
+extern void func_ov107_020c9264(int a, int b, int c);
+extern void func_ov107_020c5af8(int a, int b, int c, int d);
+extern void func_0203c634(void *node, int idx, void *cb);
+
+struct vec3 { int a, b, c; };
+extern struct vec3 data_02041dc8;
+
+void func_ov174_020d20b8(int *node) {
+    int *state = (int *)node[1];
+    *(struct vec3 *)(state + 0xb) = data_02041dc8;
+    if (state[4] != 0) {
+        VEC_Subtract((void *)(*state + 0x74), (void *)(state[4] + 0x74), state + 0xb);
+        state[0xc] = 0;
+    }
+    func_01ff8d18(state + 0xb, state + 0xb);
+    func_01ffa724(0x800, state + 0xb, state + 0xb);
+    func_ov107_020c9264(*state, 6, 0);
+    func_ov107_020c5af8(*state, 0x141, 8, state[2]);
+    state[0x16] = 0x6000;
+    *(signed char *)(*state + 0x1c7) = 0xb;
+    func_0203c634(node, *(signed char *)(node + 8), 0);
+}
