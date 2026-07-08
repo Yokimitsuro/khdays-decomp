f6a8666f69e934bc25c6fba781442b98b3eed459
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-08 03:16:31 +0200

    Match 5 dual-hw60 node action-state callbacks (orr+bic hw60 shape)

---
+++ src/calls/func_ov281_020ce1c0.c
@@ -0,0 +1,25 @@
+extern void func_ov107_020c5af8(int a, int b, int c, int d);
+extern void func_0203c634(void *node, int idx, void *cb);
+extern void func_ov281_020ce284(void);
+
+struct ov281_LowByteFlags { unsigned bits : 8; };
+
+void func_ov281_020ce1c0(int *node) {
+    int *state = (int *)node[1];
+
+    {
+        unsigned short hw60 = *(unsigned short *)(*state + 0x60);
+        *(unsigned short *)(*state + 0x60) =
+            (hw60 & ~0xff00) | (((((unsigned int)hw60 << 0x10) >> 0x18 | 6) << 0x18) >> 0x10);
+    }
+    *(unsigned short *)(*state + 0x1ae) |= 1;
+    ((struct ov281_LowByteFlags *)(*(int *)(*state + 0x388) + 8))->bits &= ~1;
+    {
+        unsigned short hw60 = *(unsigned short *)(*state + 0x60);
+        *(unsigned short *)(*state + 0x60) =
+            (hw60 & ~0xff00) | (((((unsigned int)hw60 << 0x10) >> 0x18 | 0x40) << 0x18) >> 0x10);
+    }
+    func_ov107_020c5af8(*state, 0, 0x49, *state + 0x74);
+    state[6] = 0;
+    func_0203c634(node, *(signed char *)(node + 8), func_ov281_020ce284);
+}
