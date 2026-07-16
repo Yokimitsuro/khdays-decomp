e343680ba5d7f4bd24723bf36331ccdabe47e7c3
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-16 04:13:41 +0200

    Match func_ov006_0205606c: title-screen 7-arg forwarder (44B)

---
+++ src/overlays/ov006/calls/func_ov006_0205606c.c
@@ -0,0 +1,6 @@
+/* func_ov006_0205606c -- thin forwarder to func_ov006_020521a0 (7 args, stack tail passed through). */
+extern void func_ov006_020521a0(int a, int b, int c, int d, int e, int f, int g);
+
+void func_ov006_0205606c(int a, int b, int c, int d, int e, int f, int g) {
+    func_ov006_020521a0(a, b, c, d, e, f, g);
+}
