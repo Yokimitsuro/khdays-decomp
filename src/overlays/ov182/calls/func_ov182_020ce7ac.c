64a2a405aa613c64ecc0fbffb1f609edff479974
Yokimitsuro <gonzalezciclo@gmail.com>
2026-07-19 06:51:17 +0200

    progress: C=13575 (58.4%), bytes 29.74% -- ov147 (x5) + ov181 heads first try

---
+++ src/overlays/ov182/calls/func_ov182_020ce7ac.c
@@ -0,0 +1,23 @@
+extern void func_0203c634(int *self, int idx, void *cb);
+extern void func_ov182_020ceb34(void);
+extern void func_ov182_020ce888(void);
+extern void func_ov182_020cea80(void);
+
+struct hw60 { unsigned short lo : 8, hi : 8; };
+struct b8 { unsigned int f : 8; };
+
+void func_ov182_020ce7ac(int *self) {
+    int *s = (int *)self[1];
+    int zero = 0;
+    *(signed char *)(*s + 0x1c6) = zero;
+    *(signed char *)(*s + 0x1c7) = zero - 1;
+    ((struct b8 *)(*(int *)(*s + 0x388) + 8))->f &= ~1;
+    s[1] = *s + 0xb0;
+    s[2] = *s + 0x74;
+    s[3] = *(int *)(*s + 0x384) + 0xad;
+    *(int *)(*s + 0x394) = 1;
+    ((struct hw60 *)(*s + 0x60))->hi |= (unsigned char)6;
+    func_0203c634(self, 1, (void *)&func_ov182_020ceb34);
+    func_0203c634(self, 0, (void *)&func_ov182_020ce888);
+    func_0203c634(self, 2, (void *)&func_ov182_020cea80);
+}
---
+++ src/overlays/ov199/calls/func_ov199_020d4f30.c
@@ -0,0 +1,23 @@
+extern int func_ov107_020cab14(int a, int b);
+extern void func_ov107_020c9264(int a, int b, int c);
+extern void func_0203c634(int *self, int idx, void *cb);
+extern void func_ov199_020d5008(void);
+
+typedef struct { int x, y, z; } Vec3;
+extern Vec3 data_02041dc8;
+extern void func_ov107_020c0b90(int a, int b, Vec3 v, int d);
+
+void func_ov199_020d4f30(int *self) {
+    int *s = (int *)self[1];
+    *(int *)(*s + 0x394) = func_ov107_020cab14(*s, 0);
+    if (*(int *)(*s + 0x394) == 0) {
+        *(signed char *)(*s + 0x1c7) = 2;
+        func_0203c634(self, *(signed char *)((char *)self + 0x20), 0);
+        return;
+    }
+    func_ov107_020c9264(*s, 6, 0);
+    func_ov107_020c0b90(*s, 0, *(Vec3 *)(*s + 0x3d8), 0);
+    func_ov107_020c0b90(*s, 2, data_02041dc8, 0);
+    s[0x10] = 0;
+    func_0203c634(self, *(signed char *)((char *)self + 0x20), (void *)&func_ov199_020d5008);
+}
