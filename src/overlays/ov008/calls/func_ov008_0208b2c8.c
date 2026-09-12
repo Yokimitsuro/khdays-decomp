ebbc6f3cdc1fd4db2b4f980cc0c0e2f81b619ede
Yokimitsuro <gonzalezciclo@gmail.com>
2026-09-12 22:11:19 +0200

    Match three ov008 functions: scroll gauge knob, recipe listed test, option flag toggle; unify the recipe record names

--- src/overlays/ov008/calls/func_ov008_0208b2c8.c
+++ src/overlays/ov008/calls/func_ov008_0208b2c8.c
@@ -1,4 +1,4 @@
-/* func_ov008_0208b2c8 -- Ov008_ApplySynthesisResult: hand over the synthesised item
+/* func_ov008_0208b2c8 -- Ov008_CommitSynthesisOrder: hand over the synthesised item
  * (stock + count, its "seen" flag, the recipe's unlock bit) and take the up to four
  * ingredient stacks out of the stock.
  */
@@ -8,24 +8,24 @@ typedef unsigned int  u32;
 #define INGREDIENT_COUNT 4
 #define FLAG_ITEM_SEEN_BASE 0x4db
 
-typedef struct Ov008ParamRecord {
+typedef struct Ov008ItemDef {
     u8  pad_0000[0x14];
-    int nId;                          /* 0x14 */
-} Ov008ParamRecord;
+    int nItemId;                      /* 0x14 */
+} Ov008ItemDef;
 
 typedef struct Ov008ItemStack {
-    Ov008ParamRecord *pRecord;
-    int               nCount;
+    Ov008ItemDef *pItemDef;
+    int           nCount;
 } Ov008ItemStack;
 
-typedef struct Ov008SynthRequest {
+typedef struct Ov008ParamRecord {                  /* synthesis recipe */
     int               pad_0000;
     u32               nUnlockBit;     /* 0x04 */
-    int               pad_0008;
-    Ov008ParamRecord *pRecord;        /* 0x0c */
+    int               nPrice;         /* 0x08 */
+    Ov008ItemDef     *pItemDef;       /* 0x0c: product */
     int               nCount;         /* 0x10 */
     Ov008ItemStack    aIngredient[INGREDIENT_COUNT]; /* 0x14 */
-} Ov008SynthRequest;
+} Ov008ParamRecord;
 
 typedef struct GameState {
     u8 pad_0000[0x810];
@@ -37,16 +37,16 @@ extern GameState *data_0204be18;
 extern void func_020235a8(int nFlag);              /* GameState_SetFlag */
 extern void func_02025640(u8 *pBits, u32 nBit);     /* BitArray_SetBit */
 
-void func_ov008_0208b2c8(Ov008SynthRequest *pReq)
+void func_ov008_0208b2c8(Ov008ParamRecord *pRecipe)
 {
     int i;
 
-    data_0204be18->aItemCount[pReq->pRecord->nId] += pReq->nCount;
-    func_020235a8(pReq->pRecord->nId + FLAG_ITEM_SEEN_BASE);
-    func_02025640(data_0204be18->aUnlockBits, pReq->nUnlockBit);
+    data_0204be18->aItemCount[pRecipe->pItemDef->nItemId] += pRecipe->nCount;
+    func_020235a8(pRecipe->pItemDef->nItemId + FLAG_ITEM_SEEN_BASE);
+    func_02025640(data_0204be18->aUnlockBits, pRecipe->nUnlockBit);
     for (i = 0; i < INGREDIENT_COUNT; i++) {
-        if (pReq->aIngredient[i].nCount != 0) {
-            data_0204be18->aItemCount[pReq->aIngredient[i].pRecord->nId] -= pReq->aIngredient[i].nCount;
+        if (pRecipe->aIngredient[i].nCount != 0) {
+            data_0204be18->aItemCount[pRecipe->aIngredient[i].pItemDef->nItemId] -= pRecipe->aIngredient[i].nCount;
         }
     }
 }
